1f405ff7d02225a7b40426ed0f3566e273689de8
Felix <23635466+its-felix@users.noreply.github.com>
2023-12-14 23:19:09 +0100

    day14 part2

--- day14/day14.go
+++ day14/day14.go
@@ -1,12 +1,15 @@
 package day14
 
 func SolvePart1(input <-chan string) int {
-	return solve(tiltNorth(parse(input)))
+	lines := parse(input)
+	tiltNorth(lines)
+	return solve(lines)
 }
 
 func SolvePart2(input <-chan string) int {
-	parse(input)
-	return 0
+	lines := parse(input)
+	runCycles(lines, 1000000000)
+	return solve(lines)
 }
 
 func solve(lines [][]rune) int {
@@ -23,7 +26,47 @@ func solve(lines [][]rune) int {
 	return sum
 }
 
-func tiltNorth(lines [][]rune) [][]rune {
+func buildString(lines [][]rune) string {
+	s := ""
+	for _, line := range lines {
+		s += string(line)
+		s += "\n"
+	}
+
+	return s[:len(s)-1]
+}
+
+func runCycles(lines [][]rune, times int) {
+	var cycleStart, cycleLen int
+	cache := make(map[string]int)
+
+	for i := 1; i <= times && cycleLen == 0; i++ {
+		runCycle(lines)
+		s := buildString(lines)
+		if prev, ok := cache[s]; ok {
+			cycleStart = prev
+			cycleLen = i - prev
+		} else {
+			cache[s] = i
+		}
+	}
+
+	if cycleLen > 0 {
+		times = (times - cycleStart) % cycleLen
+		for i := 0; i < times; i++ {
+			runCycle(lines)
+		}
+	}
+}
+
+func runCycle(lines [][]rune) {
+	tiltNorth(lines)
+	tiltWest(lines)
+	tiltSouth(lines)
+	tiltEast(lines)
+}
+
+func tiltNorth(lines [][]rune) {
 	for {
 		anyMoved := false
 
@@ -41,8 +84,66 @@ func tiltNorth(lines [][]rune) [][]rune {
 			break
 		}
 	}
+}
 
-	return lines
+func tiltSouth(lines [][]rune) {
+	for {
+		anyMoved := false
+
+		for row := len(lines) - 2; row >= 0; row-- {
+			for col := 0; col < len(lines[row]); col++ {
+				if lines[row+1][col] == '.' && lines[row][col] == 'O' {
+					lines[row+1][col] = lines[row][col]
+					lines[row][col] = '.'
+					anyMoved = true
+				}
+			}
+		}
+
+		if !anyMoved {
+			break
+		}
+	}
+}
+
+func tiltWest(lines [][]rune) {
+	for {
+		anyMoved := false
+
+		for col := 1; col < len(lines[0]); col++ {
+			for row := 0; row < len(lines); row++ {
+				if lines[row][col-1] == '.' && lines[row][col] == 'O' {
+					lines[row][col-1] = lines[row][col]
+					lines[row][col] = '.'
+					anyMoved = true
+				}
+			}
+		}
+
+		if !anyMoved {
+			break
+		}
+	}
+}
+
+func tiltEast(lines [][]rune) {
+	for {
+		anyMoved := false
+
+		for col := len(lines[0]) - 2; col >= 0; col-- {
+			for row := 0; row < len(lines); row++ {
+				if lines[row][col+1] == '.' && lines[row][col] == 'O' {
+					lines[row][col+1] = lines[row][col]
+					lines[row][col] = '.'
+					anyMoved = true
+				}
+			}
+		}
+
+		if !anyMoved {
+			break
+		}
+	}
 }
 
 func parse(input <-chan string) [][]rune {
