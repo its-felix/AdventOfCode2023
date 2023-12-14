ca795645581ee4cc091c75bc538dc5ae97caec6c
Felix <23635466+its-felix@users.noreply.github.com>
2023-12-15 00:42:47 +0100

    day14 generic tilt

--- day14/day14.go
+++ day14/day14.go
@@ -1,24 +1,31 @@
 package day14
 
+const (
+	north = uint8(iota)
+	east
+	south
+	west
+)
+
 func SolvePart1(input <-chan string) int {
-	lines := parse(input)
-	tiltNorth(lines)
-	return solve(lines)
+	g := parse(input)
+	g.tilt(north)
+	return solve(g)
 }
 
 func SolvePart2(input <-chan string) int {
-	lines := parse(input)
-	runCycles(lines, 1000000000)
-	return solve(lines)
+	g := parse(input)
+	runCycles(g, 1000000000)
+	return solve(g)
 }
 
-func solve(lines [][]rune) int {
+func solve(g grid) int {
 	sum := 0
 
-	for i, line := range lines {
+	for i, line := range g {
 		for _, r := range line {
 			if r == 'O' {
-				sum += len(lines) - i
+				sum += len(g) - i
 			}
 		}
 	}
@@ -26,9 +33,9 @@ func solve(lines [][]rune) int {
 	return sum
 }
 
-func buildString(lines [][]rune) string {
+func buildString(g grid) string {
 	s := ""
-	for _, line := range lines {
+	for _, line := range g {
 		s += string(line)
 		s += "\n"
 	}
@@ -36,13 +43,13 @@ func buildString(lines [][]rune) string {
 	return s[:len(s)-1]
 }
 
-func runCycles(lines [][]rune, times int) {
+func runCycles(g grid, times int) {
 	var cycleStart, cycleLen int
 	cache := make(map[string]int)
 
 	for i := 1; i <= times && cycleLen == 0; i++ {
-		runCycle(lines)
-		s := buildString(lines)
+		runCycle(g)
+		s := buildString(g)
 		if prev, ok := cache[s]; ok {
 			cycleStart = prev
 			cycleLen = i - prev
@@ -54,29 +61,29 @@ func runCycles(lines [][]rune, times int) {
 	if cycleLen > 0 {
 		times = (times - cycleStart) % cycleLen
 		for i := 0; i < times; i++ {
-			runCycle(lines)
+			runCycle(g)
 		}
 	}
 }
 
-func runCycle(lines [][]rune) {
-	tiltNorth(lines)
-	tiltWest(lines)
-	tiltSouth(lines)
-	tiltEast(lines)
+func runCycle(g grid) {
+	g.tilt(north)
+	g.tilt(west)
+	g.tilt(south)
+	g.tilt(east)
 }
 
-func tiltNorth(lines [][]rune) {
+type grid [][]rune
+
+func (g grid) tilt(tilt uint8) {
 	for {
 		anyMoved := false
-
-		for row := 1; row < len(lines); row++ {
-			for col := 0; col < len(lines[row]); col++ {
-				if lines[row-1][col] == '.' && lines[row][col] == 'O' {
-					lines[row-1][col] = lines[row][col]
-					lines[row][col] = '.'
-					anyMoved = true
-				}
+		for idxs := range g.iter(tilt) {
+			rowPrev, colPrev, row, col := idxs[0][0], idxs[0][1], idxs[1][0], idxs[1][1]
+			if g[rowPrev][colPrev] == '.' && g[row][col] == 'O' {
+				g[rowPrev][colPrev] = 'O'
+				g[row][col] = '.'
+				anyMoved = true
 			}
 		}
 
@@ -86,67 +93,58 @@ func tiltNorth(lines [][]rune) {
 	}
 }
 
-func tiltSouth(lines [][]rune) {
-	for {
-		anyMoved := false
+func (g grid) iter(tilt uint8) <-chan [2][2]int {
+	ch := make(chan [2][2]int)
+	go func() {
+		defer close(ch)
 
-		for row := len(lines) - 2; row >= 0; row-- {
-			for col := 0; col < len(lines[row]); col++ {
-				if lines[row+1][col] == '.' && lines[row][col] == 'O' {
-					lines[row+1][col] = lines[row][col]
-					lines[row][col] = '.'
-					anyMoved = true
-				}
+		var rIdx, cIdx int
+		var prevOffset int
+		var rStart, rEnd, rIncr int
+		var cStart, cEnd, cIncr int
+
+		switch tilt {
+		case north:
+			rIdx, cIdx = 0, 1
+			prevOffset = -1
+			rStart, rEnd, rIncr = 1, len(g), 1
+			cStart, cEnd, cIncr = 0, len(g[0]), 1
+		case south:
+			rIdx, cIdx = 0, 1
+			prevOffset = 1
+			rStart, rEnd, rIncr = len(g)-2, -1, -1
+			cStart, cEnd, cIncr = 0, len(g[0]), 1
+		case east:
+			rIdx, cIdx = 1, 0
+			prevOffset = 1
+			rStart, rEnd, rIncr = len(g[0])-2, -1, -1
+			cStart, cEnd, cIncr = 0, len(g), 1
+		case west:
+			rIdx, cIdx = 1, 0
+			prevOffset = -1
+			rStart, rEnd, rIncr = 1, len(g[0]), 1
+			cStart, cEnd, cIncr = 0, len(g), 1
+		default:
+			panic("invalid tilt")
+		}
+
+		for row := rStart; row != rEnd; row += rIncr {
+			for col := cStart; col != cEnd; col += cIncr {
+				var idxs [2][2]int
+				idxs[0][rIdx] = row + prevOffset
+				idxs[0][cIdx] = col
+				idxs[1][rIdx] = row
+				idxs[1][cIdx] = col
+
+				ch <- idxs
 			}
 		}
+	}()
 
-		if !anyMoved {
-			break
-		}
-	}
+	return ch
 }
 
-func tiltWest(lines [][]rune) {
-	for {
-		anyMoved := false
-
-		for col := 1; col < len(lines[0]); col++ {
-			for row := 0; row < len(lines); row++ {
-				if lines[row][col-1] == '.' && lines[row][col] == 'O' {
-					lines[row][col-1] = lines[row][col]
-					lines[row][col] = '.'
-					anyMoved = true
-				}
-			}
-		}
-
-		if !anyMoved {
-			break
-		}
-	}
-}
-
-func tiltEast(lines [][]rune) {
-	for {
-		anyMoved := false
-
-		for col := len(lines[0]) - 2; col >= 0; col-- {
-			for row := 0; row < len(lines); row++ {
-				if lines[row][col+1] == '.' && lines[row][col] == 'O' {
-					lines[row][col+1] = lines[row][col]
-					lines[row][col] = '.'
-					anyMoved = true
-				}
-			}
-		}
-
-		if !anyMoved {
-			break
-		}
-	}
-}
-
-func parse(input <-chan string) [][]rune {
+func parse(input <-chan string) grid {
 	lines := make([][]rune, 0)
 	for line := range input {
 		lines = append(lines, []rune(line))
