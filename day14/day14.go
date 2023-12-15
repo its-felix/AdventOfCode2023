6a00e36a9e883b579f707ded8cb8d78e24322409
Felix <23635466+its-felix@users.noreply.github.com>
2023-12-15 01:07:12 +0100

    day14 generic tilt 2

--- day14/day14.go
+++ day14/day14.go
@@ -76,10 +76,18 @@ func runCycle(g grid) {
 type grid [][]rune
 
 func (g grid) tilt(tilt uint8) {
+	var rowPrev, colPrev, row, col int
+	var ok bool
+
 	for {
 		anyMoved := false
-		for idxs := range g.iter(tilt) {
-			rowPrev, colPrev, row, col := idxs[0][0], idxs[0][1], idxs[1][0], idxs[1][1]
+		iter := g.iter(tilt)
+		for {
+			rowPrev, colPrev, row, col, ok = iter()
+			if !ok {
+				break
+			}
+
 			if g[rowPrev][colPrev] == '.' && g[row][col] == 'O' {
 				g[rowPrev][colPrev] = 'O'
 				g[row][col] = '.'
@@ -93,55 +101,62 @@ func (g grid) tilt(tilt uint8) {
 	}
 }
 
-func (g grid) iter(tilt uint8) <-chan [2][2]int {
-	ch := make(chan [2][2]int)
-	go func() {
-		defer close(ch)
+func (g grid) iter(tilt uint8) func() (int, int, int, int, bool) {
+	var rIdx, cIdx int
+	var prevOffset int
+	var rStart, rEnd, rIncr int
+	var cStart, cEnd, cIncr int
 
-		var rIdx, cIdx int
-		var prevOffset int
-		var rStart, rEnd, rIncr int
-		var cStart, cEnd, cIncr int
+	switch tilt {
+	case north:
+		rIdx, cIdx = 0, 1
+		prevOffset = -1
+		rStart, rEnd, rIncr = 1, len(g), 1
+		cStart, cEnd, cIncr = 0, len(g[0]), 1
+	case south:
+		rIdx, cIdx = 0, 1
+		prevOffset = 1
+		rStart, rEnd, rIncr = len(g)-2, -1, -1
+		cStart, cEnd, cIncr = 0, len(g[0]), 1
+	case east:
+		rIdx, cIdx = 1, 0
+		prevOffset = 1
+		rStart, rEnd, rIncr = len(g[0])-2, -1, -1
+		cStart, cEnd, cIncr = 0, len(g), 1
+	case west:
+		rIdx, cIdx = 1, 0
+		prevOffset = -1
+		rStart, rEnd, rIncr = 1, len(g[0]), 1
+		cStart, cEnd, cIncr = 0, len(g), 1
+	default:
+		panic("invalid tilt")
+	}
 
-		switch tilt {
-		case north:
-			rIdx, cIdx = 0, 1
-			prevOffset = -1
-			rStart, rEnd, rIncr = 1, len(g), 1
-			cStart, cEnd, cIncr = 0, len(g[0]), 1
-		case south:
-			rIdx, cIdx = 0, 1
-			prevOffset = 1
-			rStart, rEnd, rIncr = len(g)-2, -1, -1
-			cStart, cEnd, cIncr = 0, len(g[0]), 1
-		case east:
-			rIdx, cIdx = 1, 0
-			prevOffset = 1
-			rStart, rEnd, rIncr = len(g[0])-2, -1, -1
-			cStart, cEnd, cIncr = 0, len(g), 1
-		case west:
-			rIdx, cIdx = 1, 0
-			prevOffset = -1
-			rStart, rEnd, rIncr = 1, len(g[0]), 1
-			cStart, cEnd, cIncr = 0, len(g), 1
-		default:
-			panic("invalid tilt")
+	var idxs [2][2]int
+	row, col := rStart, cStart
+	exhausted := false
+
+	return func() (int, int, int, int, bool) {
+		if exhausted {
+			return 0, 0, 0, 0, false
 		}
 
-		for row := rStart; row != rEnd; row += rIncr {
-			for col := cStart; col != cEnd; col += cIncr {
-				var idxs [2][2]int
-				idxs[0][rIdx] = row + prevOffset
-				idxs[0][cIdx] = col
-				idxs[1][rIdx] = row
-				idxs[1][cIdx] = col
+		idxs[0][rIdx] = row + prevOffset
+		idxs[0][cIdx] = col
+		idxs[1][rIdx] = row
+		idxs[1][cIdx] = col
 
-				ch <- idxs
+		col += cIncr
+		if col == cEnd {
+			col = cStart
+			row += rIncr
+			if row == rEnd {
+				exhausted = true
 			}
 		}
-	}()
 
-	return ch
+		return idxs[0][0], idxs[0][1], idxs[1][0], idxs[1][1], true
+	}
 }
 
 func parse(input <-chan string) grid {
