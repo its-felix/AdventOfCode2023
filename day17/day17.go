74847fd0743bb0535e50925af18995dc5519ca12
Felix <23635466+its-felix@users.noreply.github.com>
2023-12-18 03:23:14 +0100

    day17 improvements

--- day17/day17.go
+++ day17/day17.go
@@ -19,106 +19,35 @@ type node struct {
 	connected [4]*node
 }
 
-type traverseStateP1 struct {
-	n              *node
-	direction      int
-	directionCount int
-	cost           uint64
-	past4          [4]*node
+type pastN interface {
+	[4]*node | [11]*node
 }
 
-type traverseStateP2 struct {
+type traverseState[P pastN] struct {
 	n              *node
 	direction      int
 	directionCount int
 	cost           uint64
-	past11         [11]*node
+	pastN          P
 }
 
 func SolvePart1(input <-chan string) uint64 {
 	start, end := parse(input)
-	return traversePart1(start, end)
+	return traverse[[4]*node](start, end, 1, 3)
 }
 
 func SolvePart2(input <-chan string) uint64 {
 	start, end := parse(input)
-	return traversePart2(start, end)
+	return traverse[[11]*node](start, end, 4, 10)
 }
 
-func traversePart1(start, end *node) uint64 {
-	const maxSameDirection = 3
-
+func traverse[P pastN](start, end *node, minSameDirection, maxSameDirection int) uint64 {
 	minFinalCost := uint64(math.MaxUint64)
-	minEntryCost := make(map[[4]*node]uint64)
-	queue := make([]traverseStateP1, 0)
+	minEntryCost := make(map[P]uint64)
+	queue := make([]traverseState[P], 0)
 
 	for _, direction := range []int{east, south} {
-		queue = append(queue, traverseStateP1{
-			n:              start,
-			direction:      direction,
-			directionCount: 1,
-			cost:           0,
-		})
-	}
-
-	for len(queue) > 0 {
-		state := queue[0]
-		queue = queue[1:]
-
-		if state.n == end {
-			if state.cost < minFinalCost {
-				minFinalCost = state.cost
-			}
-			continue
-		}
-
-		for direction, conn := range state.n.connected {
-			if conn == nil || !conn.valid || direction == opposite(state.direction) {
-				continue
-			}
-
-			cost := state.cost + conn.cost
-			if cost+distance(conn, end) >= minFinalCost {
-				continue
-			}
-
-			past4 := [4]*node{conn, state.past4[0], state.past4[1], state.past4[2]}
-			if mCost, ok := minEntryCost[past4]; ok && cost >= mCost {
-				continue
-			}
-
-			directionCount := 1
-			if direction == state.direction {
-				directionCount = state.directionCount + 1
-				if directionCount > maxSameDirection {
-					continue
-				}
-			}
-
-			minEntryCost[past4] = cost
-			queue = append(queue, traverseStateP1{
-				n:              conn,
-				direction:      direction,
-				directionCount: directionCount,
-				cost:           cost,
-				past4:          past4,
-			})
-		}
-	}
-
-	return minFinalCost
-}
-
-func traversePart2(start, end *node) uint64 {
-	const minSameDirection = 4
-	const maxSameDirection = 10
-
-	minFinalCost := uint64(math.MaxUint64)
-	minEntryCost := make(map[[11]*node]uint64)
-	queue := make([]traverseStateP2, 0)
-
-	for _, direction := range []int{east, south} {
-		queue = append(queue, traverseStateP2{
+		queue = append(queue, traverseState[P]{
 			n:              start,
 			direction:      direction,
 			directionCount: 1,
@@ -147,20 +76,13 @@ func traversePart2(start, end *node) uint64 {
 				continue
 			}
 
-			past11 := [11]*node{
-				conn,
-				state.past11[0],
-				state.past11[1],
-				state.past11[2],
-				state.past11[3],
-				state.past11[4],
-				state.past11[5],
-				state.past11[6],
-				state.past11[7],
-				state.past11[8],
-				state.past11[9],
+			var pastN P
+			pastN[0] = conn
+			for i := 0; i < len(state.pastN)-1; i++ {
+				pastN[i+1] = state.pastN[i]
 			}
-			if mCost, ok := minEntryCost[past11]; ok && cost >= mCost {
+
+			if mCost, ok := minEntryCost[pastN]; ok && cost >= mCost {
 				continue
 			}
 
@@ -174,13 +96,13 @@ func traversePart2(start, end *node) uint64 {
 				continue
 			}
 
-			minEntryCost[past11] = cost
-			queue = append(queue, traverseStateP2{
+			minEntryCost[pastN] = cost
+			queue = append(queue, traverseState[P]{
 				n:              conn,
 				direction:      direction,
 				directionCount: directionCount,
 				cost:           cost,
-				past11:         past11,
+				pastN:          pastN,
 			})
 		}
 	}
